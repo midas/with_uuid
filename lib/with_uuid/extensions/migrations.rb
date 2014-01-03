d3727379fde102c838751d3fb20e4491e2c5575e
Derek Atkins <derek@ihtfp.com>
2014-01-03 14:02:16 -0500

    Add support for Rails4; the TableDefinition doesn't pass the base class, so guess it based on the @native table

--- lib/with_uuid/extensions/migrations.rb
+++ lib/with_uuid/extensions/migrations.rb
@@ -8,14 +8,24 @@ module WithUuid
       def uuid(*column_names)
         options = column_names.extract_options!
         column_names.each do |name|
-          type = case(@base.adapter_name.downcase)
-                 when 'sqlserver'
-                   'uniqueidentifier'
-                 when 'postgresql'
-                   'uuid'
-                 else
-                   'varchar(36)'
-                 end
+          type = 'varchar(36)'
+          # Handle Table (and TableDefinition in Rails 3)
+          if @base
+            case(@base.adapter_name.downcase)
+            when 'sqlserver'
+              type = 'uniqueidentifier'
+            when 'postgresql'
+              type = 'uuid'
+            end
+          # Handle TableDefinition in Rails 4
+          elsif @native
+            if @native[:uuid] #postgres in 4.0 already defines uuid
+              type = @native[:uuid].is_a?(Hash) ? @native[:uuid][:name] : 'uuid'
+            elsif @native[:ss_timestamp]  #sqlserver
+              type = 'uniqueidentifier'
+            end
+          end
+
           column(name, "#{type}#{' PRIMARY KEY' if options.delete(:primary_key)}", options)
         end
       end
